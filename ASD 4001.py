bbae50f92fa2a1dad5d8b8cbc88258a0d4dccd29
juju5473 <ankitjhurani@gmail.com>
2026-01-18 15:40:32 -0500

    initial commit

---
+++ ASD 4001.py
@@ -0,0 +1,7 @@
+import numpy
+import matplotlib.pyplot as plt
+
+# create a numpy array and learn some of its attributes
+a = numpy.array([-1,3,5,7,9,20,45,14]) 
+
+print("The numpy array a is: ", a)
---
+++ ASD4001_1.py
@@ -0,0 +1,6 @@
+import numpy
+from matplotlib import pyplot as plt
+
+a = numpy.array([-1,3,5,7,9,20,45,14]) 
+
+print("The numpy array a is: ", a)
